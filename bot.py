7efd9cc6cd744b91a04ea1cb3f428ae8a271ce35
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-10-07 17:23:11 +0500

    LIKE

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# export 
 from telegram import Update
 from telegram.ext import (
     CallbackContext, 
@@ -7,3 +8,27 @@ from telegram.ext import (
     Filters
     )
 import os
+TOKEN = os.environ["TOKEN"]
+def start(update:Update,context:CallbackContext):
+    bot = context.bot
+    chat_id = update.message.chat.id
+    bot.sendMessage(chat_id,"Welcome to Bot!")
+
+def echo(update:Update,context:CallbackContext):
+    bot = context.bot
+    text = update.message.text
+    chat_id = update.message.chat.id
+
+    # count = "Like: 1\nDislike: 1"
+    count=1
+    if count=="👍":
+        count= f"Like: {count+1}\nDislike: {count+1}"
+        return count
+    bot.sendMessage(chat_id,count)
+
+updater = Updater(token = TOKEN)
+updater.dispatcher.add_handler(CommandHandler("start",start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text,echo))
+
+updater.start_polling()
+updater.idle()
